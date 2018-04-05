967a76b8d11a81b8dfa78ac498a96905c121c8dd
2alin <alinattus@gmail.com>
2018-04-05 18:17:08 -0500

    finished v0.2

--- popup/match-tabs.js
+++ popup/match-tabs.js
@@ -10,8 +10,26 @@ var presetsDefault = {
     startIsOn: true,
     startValue: "",
     endIsOn: true,
-    endValue: ""
+    // next properties are used to filter the trailing part of the url
+    //splitChars are used to define a series of key Fields to consider
+    splitChars: {'$': true, '.': true, '#': false, '?': false},
+    //keyWord is assigned by the used as another field to filter consider
+    keyWord: {value: "", isOn: false},
+    includedWords: [],
+    excludedWords: []
   }
+}
+
+function validate(presetsTest){
+  //verify existence of auto preset
+  if (!presetsTest.hasOwnProperty("auto")){
+    return false;
+  }
+  //verify length of auto preset object
+  if (Object.keys(presetsTest.auto).length != Object.keys(presetsDefault.auto).length){
+    return false;
+  }
+  return true
 }
 
 /*
@@ -28,16 +46,15 @@ function savePresets(){
 function restorePresets() {
 
   function setPresets(result) {
-    if(result.hasOwnProperty("presetsLocal")){
+    if(result.hasOwnProperty("presetsLocal") && validate(result.presetsLocal)){
       // there's presets saved locally already
-      console.log("I exist already");
+      
       presets = result.presetsLocal;
     } else{
       // no presets saved yet
-      console.log("sorry I don't exist");
+      
       presets = presetsDefault;
     }
-    console.log(presets);
     savePresets();
     updateAutoPreset();
     fillAutoResults();
@@ -75,12 +92,27 @@ endSelect.addEventListener('click', () => {
   savePresets();
 });  
 
-
+/*
+retrieving trailing part of an url
+*/
+function endingURL(url){
+  let ending = ""
+  let re = new RegExp('(/[^/]*)$', 'i');
+  if (re.test(url)){
+    ending = re.exec(url)[0];
+    //removing the first character '/'
+    ending = ending.slice(1);
+  } else if (url.startsWith("about:")) {
+    ending = url.slice(6);
+  }
+  return ending;
+}
 
 
 /*
   update auto preset properties using active url
 */
+
 async function updateAutoPreset(){
   let thisTab = (await browser.tabs.query({active:true, currentWindow:true}))[0];
 
@@ -95,28 +127,88 @@ async function updateAutoPreset(){
     presets.auto.startValue = re.exec(thisTab.url)[0];
   }
   
-  // retrieving auto end query
-  // breakpoints chars are: '/' and '$'
-  let re = new RegExp('([.$][a-z]+)$' , 'i');
-  if (re.test(thisTab.url)){
-    presets.auto.endValue = re.exec(thisTab.url)[0];
-  } else {
-    //it has no end-breakpoints
-    presets.auto.endValue = "";
+  let ending = endingURL(thisTab.url);
+
+  // getting splitter characters
+  let splitChars = Object.keys(presets.auto.splitChars);
+  presets.auto.includedWords = [];
+  presets.auto.excludedWords = [];
+
+  // retrieving trailing text (property) for every character
+  // and classify them
+  for (char of splitChars) {
+    let property = "";
+    let re;
+    //find last index of char found
+    let lastIndex = ending.lastIndexOf(char);
+    switch(char){
+      case '.': re = new RegExp('(\\.[^\\.\\$#\\?]*)'); break;
+      case '$': re = new RegExp('(\\$[^\\.\\$#\\?]*)'); break;
+      case '#': re = new RegExp('(#[^\\.\\$#\\?]*)'); break;
+      case '?': re = new RegExp('(\\?[^\\.\\$#\\?]*)');
+
+    }
+    
+    if (re.test(ending.slice(lastIndex))){
+      property = re.exec(ending.slice(lastIndex))[0];
+    }
+    
+    //splitting the properties found before
+    //into two categories "endValuesIncluded"/"endValuesExcluded"
+    if(property === ""){
+      continue;
+    }
+    if (presets.auto.splitChars[char]){
+      presets.auto.includedWords.push(property.toLowerCase());
+    } else {
+      presets.auto.excludedWords.push(property.toLowerCase());
+    }
   }
 
+  // conside keyword and its status
+  let keyWordValue = presets.auto.keyWord.value;
+  if(presets.auto.keyWord.isOn){
+    if ( keyWordValue != ""){
+      presets.auto.includedWords.push(keyWordValue.toLowerCase());
+    }
+  } else {
+    if ( keyWordValue != ""){
+      presets.auto.excludedWords.push(keyWordValue.toLowerCase());
+    }
+  }
   updatePopup();
 }
+
+
 
 /*
   update tags and checkboxes in popup using presets
  */
 function updatePopup() {
-  console.log("popup updated");
   //filling tags in popup
-  document.querySelector('#start-select + span').textContent = presets.auto.startValue;
-  console.log(presets.auto.startValue);
-  document.querySelector('#end-select + span').textContent = presets.auto.endValue;
+  document.querySelector('#start').textContent = presets.auto.startValue;
+
+  let includeText = "";
+  for (word of presets.auto.includedWords){
+    let finalWord = word;
+    if (word.length > 8 ){
+      finalWord = word.slice(0,6) + '...';
+    }
+    includeText += finalWord + ' '; 
+  }
+
+  document.querySelector('#include').textContent = includeText;
+
+  let excludeText = "";
+  for (word of presets.auto.excludedWords){
+    let finalWord = word;
+    if (word.length > 10 ){
+      finalWord = word.slice(0,8) + '...';
+    }
+    excludeText += finalWord + ' '; 
+  }
+
+  document.querySelector('#ignore').textContent = excludeText;
 
   //filling checkboxes
   startSelect.checked = presets.auto.startIsOn;
@@ -128,35 +220,39 @@ function updatePopup() {
 /*
   fill 'matching tabs' result list
 */
+
 async function fillAutoResults(){
-  let tabs = await browser.tabs.query({});
-
-  // creating a list of tabs that matches the auto queries
-  let foundTabs = [];
-  let startIsOn = presets.auto.startIsOn;
-  let startValue = presets.auto.startValue;
-  let endIsOn = presets.auto.endIsOn;
-  let endValue = presets.auto.endValue;
-  
-  for (let tab of tabs){
-    //considering all 4 states of auto checkboxes
-    if (startIsOn && endIsOn){
-      if (tab.url.startsWith(startValue) && tab.url.endsWith(endValue)){
-        foundTabs.push(tab);
+  let foundTabs = await browser.tabs.query({});
+  let tempoTabs;
+  // first filter: 'leading' query
+  tempoTabs = [];
+  if (presets.auto.startIsOn && (presets.auto.startValue != "")) {
+    for (let tab of foundTabs){
+      if (tab.url.startsWith(presets.auto.startValue)){
+        tempoTabs.push(tab);
       }
-    } else if (startIsOn && !endIsOn){
-      if (tab.url.startsWith(startValue)){
-        foundTabs.push(tab);
-      }
-    } else if (!startIsOn && endIsOn){
-      if (tab.url.endsWith(endValue)){
-        foundTabs.push(tab);
-      }
-    } else {
-      foundTabs.push(tab);
     }
+    foundTabs = tempoTabs;
   }
-
+  
+  // second filter: 'trailing' query
+  tempoTabs = [];
+  if (presets.auto.endIsOn && (presets.auto.includedWords.length != 0)){
+    for (let tab of foundTabs){
+      let ending = endingURL(tab.url).toLowerCase();
+      
+      for (let word of presets.auto.includedWords){
+        
+        if (ending.includes(word)){
+          tempoTabs.push(tab);
+          
+          break;
+        }
+      }
+    }
+    foundTabs = tempoTabs;
+  }
+  
 
   // displays a list of tab titles with ids matching
   // the tab id, as well as adding listener to them that
@@ -210,6 +306,56 @@ browser.tabs.onActivated.addListener(() => {
   updateAutoPreset();
   fillAutoResults();
 } );
+
+
+/*
+  Settings popup window
+*/
+
+// open settings popup when clicking settings button
+var settingsBtn = document.querySelector('#settings');
+var settingsPopup = document.querySelector('#settings-popup');
+settingsBtn.addEventListener('click',() => {
+  updateSettingsForm();
+  settingsPopup.className = 'visible';
+});
+//handles save button
+var settingsForm = document.querySelector('#settings-form');
+settingsForm.addEventListener('submit', ()=> {
+  settingsPopup.className = "";
+  updateSettingsPreset();
+  savePresets();
+  updateAutoPreset();
+  fillAutoResults();
+  
+});
+//handles reset button
+settingsForm.addEventListener('reset', (e) => {
+  e.preventDefault();
+  presets = presetsDefault;
+  savePresets();
+  updateAutoPreset();
+  fillAutoResults();
+  updateSettingsForm();
+});
+
+function updateSettingsForm(){
+  document.querySelector('#dollar').checked = presets.auto.splitChars['$'];
+  document.querySelector('#dot').checked = presets.auto.splitChars['.'];
+  document.querySelector('#sharp').checked = presets.auto.splitChars['#'];
+  document.querySelector('#question').checked = presets.auto.splitChars['?'];
+  document.querySelector('#keyword-text').value = presets.auto.keyWord.value;
+  document.querySelector('#keyword-check').checked = presets.auto.keyWord.isOn;
+}
+function updateSettingsPreset(){
+  presets.auto.splitChars['$'] = settingsForm.elements['dollar'].checked;
+  presets.auto.splitChars['.'] = settingsForm.elements['dot'].checked;
+  presets.auto.splitChars['#'] = settingsForm.elements['sharp'].checked;
+  presets.auto.splitChars['?'] = settingsForm.elements['question'].checked;
+  presets.auto.keyWord.value = settingsForm.elements['keyword-text'].value;
+  presets.auto.keyWord.isOn = settingsForm.elements['keyword-check'].checked;
+}
+
 
 
 
