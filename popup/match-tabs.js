33cb63e811a73929949f5c1c8fb0853fdda9c149
2alin <alinattus@gmail.com>
2018-04-14 22:37:28 -0500

    Add new 'search' mode; Add helping elements; Add Style

--- popup/match-tabs.js
+++ popup/match-tabs.js
@@ -6,6 +6,7 @@
 
 var presetsDefault = {
   modeActive: "auto",
+  // presets object version, independent of extension's version
   version: "1.0",
   auto: {
     startIsOn: true,
@@ -18,6 +19,9 @@ var presetsDefault = {
     keyWord: {value: "", isOn: false},
     includedWords: [],
     excludedWords: []
+  },
+  search: {
+    query: ""
   }
 }
 
@@ -29,16 +33,7 @@ function validate(presetsTest){
     return false;
   }
   return true;
-  //next code deprecated because of version property
-  /* //verify existence of auto preset
-  if (!presetsTest.hasOwnProperty("auto")){
-    return false;
-  }
-  //verify length of auto preset object
-  if (Object.keys(presetsTest.auto).length != Object.keys(presetsDefault.auto).length){
-    return false;
-  }
-  return true */
+
 }
 
 
@@ -86,18 +81,31 @@ document.addEventListener("DOMContentLoaded", restorePresets);
 
 
 /*
-  Nav Bar actions
+  Nav Bar actions and mode form variables
 */
+
 var autoBtn = document.querySelector('#auto-btn');
 var searchBtn = document.querySelector('#search-btn');
+var autoForm = document.querySelector('form.auto');
+var searchForm = document.querySelector('form.search');
+var foundTabs = document.querySelector('#found-tabs');
+var searchTabs = document.querySelector('#search-tabs');
+var autoHelper = document.querySelector("div.helper.auto")
+var searchHelper = document.querySelector("div.helper.search");
 
 autoBtn.addEventListener("click", () => {
-  autoBtn.className = "active";
-  searchBtn.className = "";
+  // autoBtn.className = "active";
+  // searchBtn.className = "";
+  presets.modeActive = "auto";
+  updatePopup();
+  savePresets();
 });
 searchBtn.addEventListener("click", () => {
-  searchBtn.className = "active";
-  autoBtn.className = "";
+  // searchBtn.className = "active";
+  // autoBtn.className = "";
+  presets.modeActive = "search";
+  updatePopup();
+  savePresets();
 });
 
 
@@ -120,6 +128,83 @@ endSelect.addEventListener('click', () => {
   savePresets();
 });  
 
+
+/*
+  binding search text form to preset properties
+*/
+
+var searchField = document.querySelector('#search-query');
+
+
+//adding event listener when typing
+searchField.addEventListener('input', () => {
+  
+  presets.search.query = searchField.value.toLowerCase().trim();
+  savePresets();
+  
+  // calling find tabs algorithm
+  searchByText();
+})
+
+
+/*
+  binding backspace button to its proper action
+*/
+
+var backspaceBtn = document.querySelector('#backspace');
+
+backspaceBtn.addEventListener('click', () => {
+  searchField.value = "";
+  presets.search.query = "";
+  savePresets();
+  searchByText();
+  searchField.focus();
+});
+
+
+/*
+  binding up/down arrow keys for list navigation
+*/
+
+document.addEventListener('keydown', (e) => {
+  let keyCode = e.keyCode;
+  let listTabsActive = document.querySelector('div.list-tabs.active');
+
+  if(!listTabsActive.classList.contains('empty')){
+    let list = document.querySelector('div.list-tabs.active ol');
+    let firstElement = list.firstChild;
+    let lastElement = list.lastChild;
+    let focusedElement = document.activeElement;
+    if (keyCode === 38){
+      // UP key pressed
+      e.preventDefault(); //prevent scrollbars to move with arrow keys
+      if (focusedElement === firstElement ){
+        //current element is first in the list
+        lastElement.focus();
+      } else if (focusedElement.parentNode === list) {
+        //current element is in the list, not first
+        focusedElement.previousSibling.focus();
+      } else {
+        //current element is something not in the list
+        lastElement.focus();
+      }
+    } else if (keyCode === 40){
+      // UP key pressed
+      e.preventDefault(); //prevent scrollbars to move with arrow keys
+      if (focusedElement === lastElement ){
+        //current element is last in the list
+        firstElement.focus();
+      } else if (focusedElement.parentNode === list) {
+        //current element is in the list, not first
+        focusedElement.nextSibling.focus();
+      } else {
+        //current element is something not in the list
+        firstElement.focus();
+      }
+    }
+  }
+
+});
 
 
 /*
@@ -217,35 +302,88 @@ async function updateAutoPreset(){
  */
 
 function updatePopup() {
-  //filling tags in popup
-  document.querySelector('#start').textContent = presets.auto.startValue;
+  
+  if (presets.modeActive === 'auto'){
+    ////////////////////////// 
+    // auto mode activated //
 
-  let includeText = "";
-  for (word of presets.auto.includedWords){
-    let finalWord = word;
-    if (word.length > 8 ){
-      finalWord = word.slice(0,6) + '...';
+
+    // set nav and form 'active' classes
+
+    autoForm.classList.add('active');
+    searchForm.classList.remove('active');
+    foundTabs.classList.add('active');
+    searchTabs.classList.remove('active');
+    //set active nav button when doc loads
+    autoBtn.classList.add('active');
+    searchBtn.classList.remove('active');
+    //remove remaining helpers
+    searchHelper.classList.remove('active');
+
+    //handling exception active autohelper
+    if (foundTabs.classList.contains("empty")){
+      autoHelper.classList.add('active');
     }
-    includeText += finalWord + ' '; 
+    
+
+
+    // filling auto form //
+
+
+    // start auxiliary text
+    document.querySelector('#start').textContent = presets.auto.startValue;
+
+    // end auxiliary text
+    let includeText = "";
+    for (word of presets.auto.includedWords){
+      let finalWord = word;
+      if (word.length > 8 ){
+        finalWord = word.slice(0,6) + '...';
+      }
+      includeText += finalWord + ' '; 
+    }
+    document.querySelector('#include').textContent = includeText;
+
+    //filling checkboxes
+    startSelect.checked = presets.auto.startIsOn;
+    endSelect.checked = presets.auto.endIsOn;
+
+    // //updating filtered list
+    // fillAutoResults();
+
+    //focus html for arrow keys to scroll list inmediately
+    document.querySelector('html').focus();
+
+
+
+  } else {
+    ////////////////////////////
+    // search mode activated //
+
+
+    // set nav and form 'active' classes
+
+    searchForm.classList.add('active');
+    autoForm.classList.remove('active');
+    searchTabs.classList.add('active');
+    foundTabs.classList.remove('active');
+    //set active nav button when doc loads
+    searchBtn.classList.add('active');
+    autoBtn.classList.remove('active');
+    //remove remaining helpers
+    autoHelper.classList.remove('active');
+
+    // restore query search from presets
+    searchField.value = presets.search.query;
+  
+    // fill search mode tab list
+    searchByText();
+
+    //focus searchField for arrow keys to scroll list inmediately
+    searchField.focus();
   }
 
-  document.querySelector('#include').textContent = includeText;
-
-  let excludeText = "";
-  for (word of presets.auto.excludedWords){
-    let finalWord = word;
-    if (word.length > 10 ){
-      finalWord = word.slice(0,8) + '...';
-    }
-    excludeText += finalWord + ' '; 
-  }
-
-  // exclude field auxiliary text was deprecated since v1.0
-  // document.querySelector('#ignore').textContent = excludeText;
-
-  //filling checkboxes
-  startSelect.checked = presets.auto.startIsOn;
-  endSelect.checked = presets.auto.endIsOn;
+  
 }
 
 
@@ -255,6 +393,7 @@ function updatePopup() {
 */
 
 async function fillAutoResults(){
+  
   let foundTabs = await browser.tabs.query({});
   let tempoTabs;
   // first filter: 'leading' query
@@ -289,12 +428,13 @@ async function fillAutoResults(){
 
   // displays a list of tab titles with ids matching
   // the tab id, as well as adding listener to them that
-  // will change to its respectie tab when click over them
+  // will change to its respective tab when click over them
   //
   // For a reason, I got the items in 'ol' un-numbered, so 
   // I'm using 'k' as a counter
   let resultsBox = document.querySelector("#found-tabs");
   resultsBox.innerHTML = "";
+
   let resultsList = document.createElement("ol");
 
   let thisTab = (await browser.tabs.query({active:true, currentWindow:true}))[0];
@@ -337,7 +477,11 @@ async function fillAutoResults(){
       tabItem.className = 'active';
     }
 
-    tabItem.addEventListener('click', (e) => {
+    //adding listener for click events
+    tabItem.addEventListener('click', changeTab);
+
+    //change tab function
+    function changeTab(e) {
       let id;
       let windowId;
       if (e.target.nodeName.toLowerCase() === "li"){
@@ -350,24 +494,47 @@ async function fillAutoResults(){
       browser.windows.update(windowId, {focused: true});
       browser.tabs.update(id, {active: true});
       window.close();
+    }
+
+    //adding attribute for arrow key focus
+    tabItem.setAttribute("tabindex", "1");
+
+    //adding listener for enter key down
+    tabItem.addEventListener('keydown', (e) => {
+      if (e.keyCode === 13){
+        changeTab(e);
+      }
     });
+  
   }
 
   resultsBox.appendChild(resultsList);
   
   // Dynamic list counter width
-  resultsBox.className = "";
+  // resultsBox.className = "";
+  resultsBox.classList.remove('empty');
+  resultsBox.classList.remove('one-digit');
+  resultsBox.classList.remove('two-digits');
+  resultsBox.classList.remove('three-digits');
   if (k === 0){
-    resultsBox.className = "empty";
+    resultsBox.classList.add('empty');
   } else if (k < 10){
-    resultsBox.className = "one-digit";
+    resultsBox.classList.add('one-digit');
   } else if(k < 100){
-    resultsBox.className = "two-digits";
+    resultsBox.classList.add('two-digits');
   } else {
-    resultsBox.className = "three-digits";
+    resultsBox.classList.add('three-digits');
+  }
+
+  // if list is empty, activate auto helper window
+  if(resultsBox.classList.contains('empty') && presets.modeActive === "auto"){
+    autoHelper.classList.add('active');
+  } else{
+    autoHelper.classList.remove('active');
   }
 
 }
+
 
 
 /* 
@@ -426,6 +593,162 @@ function updateSettingsPreset(){
   presets.auto.splitChars['?'] = settingsForm.elements['question'].checked;
   presets.auto.keyWord.value = settingsForm.elements['keyword-text'].value.trim();
   presets.auto.keyWord.isOn = settingsForm.elements['keyword-check'].checked;
+}
+
+
+/* 
+search tabs by text algorithm
+*/
+
+async function searchByText() {
+  let searchText = presets.search.query;
+  let allTabs = await browser.tabs.query({});
+  let resultTabs = []
+
+  // take out trailing commas, to avoid  showing the whole
+  // list everytime we add an OR word
+  if (searchText.endsWith(',')){
+    searchText = searchText.slice(0,-1);
+  }
+
+  // process the search Text
+  // w1 w2, w3 w4 => [[w1, w2], [w3,w4]] = (w1 and w2) or (w3 or w4)
+  let orList = searchText.split(',');
+  let finalList = [];
+  for (let text of orList){
+    finalList.push(text.trim().split(' '));
+  }
+  
+
+  ///////////////////// 
+  // filter process //
+
+  for (let tab of allTabs) {
+    let isMatch = false;
+    let title = tab.title.toLowerCase();
+    let url = tab.url.toLowerCase();
+
+    for (orWords of finalList){
+      isMatch = true;
+      for (word of orWords){
+        if(!title.includes(word) && !url.includes(word)){
+          isMatch = false;
+          break;
+        }
+      }
+      if (isMatch){
+        break;
+      }
+    }
+
+    // tab is a match, and added to the result list
+    if (isMatch){
+      resultTabs.push(tab);
+    }
+
+  }
+  
+  /////////////////////////
+  // display the results //
+
+  let resultsBox = document.querySelector("#search-tabs");
+  resultsBox.innerHTML = "";
+  let resultsList = document.createElement("ol");
+
+  let thisTab = (await browser.tabs.query({active:true, currentWindow:true}))[0];
+  
+  let k = 0; // counter for list item
+
+  for (let tab of resultTabs){
+    let tabItem = document.createElement('li');
+    let itemCounter = document.createElement('span');
+    let favIcon = document.createElement('span');
+    let titleTab = document.createElement('span');
+    
+    k++; // counter for list item
+    itemCounter.textContent = k + '.'; // fill itemCounter
+
+    // fill favicon
+    if(tab.favIconUrl){
+      if(tab.favIconUrl.startsWith("http")){
+        favIcon.style.backgroundImage = 'url(' + tab.favIconUrl +')';
+      }
+    }
+
+    titleTab.textContent = tab.title// fill tab title
+    
+    // form list item
+    tabItem.appendChild(itemCounter); 
+    tabItem.appendChild(favIcon);
+    tabItem.appendChild(titleTab);
+
+
+    resultsList.appendChild(tabItem); //append to the list
+
+    // assign tab id and window ID as id and class of tabItem
+    // in order to inherit such values
+    tabItem.id = tab.id;
+    tabItem.className =tab.windowId;
+    
+    //add special class to item related to active tab
+    if (tab.id == thisTab.id ){
+      tabItem.className = 'active';
+    }
+
+    //ading listener for click events
+    tabItem.addEventListener('click', changeTab);
+
+    // change tab function
+    function changeTab(e) {
+      let id;
+      let windowId;
+      if (e.target.nodeName.toLowerCase() === "li"){
+        id = Number(e.target.id);
+        windowId = Number(e.target.className);
+      } else {
+        windowId = Number(e.target.parentNode.className);
+        id = Number(e.target.parentNode.id);
+      }
+      browser.windows.update(windowId, {focused: true});
+      browser.tabs.update(id, {active: true});
+      window.close();
+    }
+
+    //adding attribute for arrow key focus
+    tabItem.setAttribute("tabindex", "1");
+
+    //adding listener for enter key down
+    tabItem.addEventListener('keydown', (e) => {
+      if (e.keyCode === 13){
+        changeTab(e);
+      }
+    });
+  }
+
+  resultsBox.appendChild(resultsList);
+  
+  // Dynamic list counter width
+  resultsBox.classList.remove('empty');
+  resultsBox.classList.remove('one-digit');
+  resultsBox.classList.remove('two-digits');
+  resultsBox.classList.remove('three-digits');
+  if (k === 0){
+    resultsBox.classList.add('empty');
+  } else if (k < 10){
+    resultsBox.classList.add('one-digit');
+  } else if(k < 100){
+    resultsBox.classList.add('two-digits');
+  } else {
+    resultsBox.classList.add('three-digits');
+  }
+
+
+  // if list is empty, activate search helper window
+  if(resultsBox.classList.contains('empty') && presets.modeActive === "search"){
+    searchHelper.classList.add('active');
+  } else{
+    searchHelper.classList.remove('active');
+  }
 }
 
 
